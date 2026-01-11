034f173363f1dcf20c3f8a13a4a39f73b133f525
emirbesir <emirbesirx@gmail.com>
2026-01-11 17:41:59 +0300

    chore: translate featured to turkish

--- js/main.js
+++ js/main.js
@@ -150,7 +150,7 @@ function createProjectCard(project, index) {
         <div class="project-image">
             <img src="${project.thumbnail}" alt="${project.title} thumbnail" 
                  onerror="this.src='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 400 225%22><rect fill=%22%231a1a2e%22 width=%22400%22 height=%22225%22/><text x=%22200%22 y=%22120%22 fill=%22%2300d4ff%22 font-family=%22sans-serif%22 font-size=%2220%22 text-anchor=%22middle%22>${project.title}</text></svg>'">
-            ${project.featured ? '<span class="featured-badge"><i class="fas fa-star"></i> Featured</span>' : ''}
+            ${project.featured ? '<span class="featured-badge"><i class="fas fa-star"></i> Öne Çıkarılan</span>' : ''}
         </div>
         <div class="project-content">
             <div class="project-tags">${tagsHTML}</div>
